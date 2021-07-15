4760bff02db036c93c738b5a1e1c89373d90275f
RamirooMendozaSanchez <ramiro_mendoza@arpan.com.mx>
2021-07-15 14:55:42 -0500

    Initial commit

---
+++ frontend/vistas/modulos/destacados.html.php
@@ -0,0 +1,358 @@
+
+
+<!--=========================================
+Este codigo no funciona solo es un ejemplo
+el codigo que funciona esta en
+destacados.php
+===========================================-->
+
+
+
+<!--=====================================
+BANNER
+======================================-->
+
+<!--<figure class="banner">
+</figure>-->
+
+<!--=====================================
+BARRA PRODUCTOS GRATIS
+======================================-->
+
+<!--<div class="container-fluid well well-sm barraProductos">
+
+	<div class="container">
+		
+		<div class="row">
+			
+			<div class="col-xs-12 organizarProductos">
+
+				<div class="btn-group pull-right">
+
+					 <button type="button" class="btn btn-default btnGrid" id="btnGrid0">
+					 	
+						<i class="fa fa-th" aria-hidden="true"></i>  
+
+						<span class="col-xs-0 pull-right"> GRID</span>
+
+					 </button>
+
+					 <button type="button" class="btn btn-default btnList" id="btnList0">
+					 	
+						<i class="fa fa-list" aria-hidden="true"></i> 
+
+						<span class="col-xs-0 pull-right"> LIST</span>
+
+					 </button>
+					
+				</div>		
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>-->
+
+<!--=====================================
+VITRINA DE PRODUCTOS GRATIS
+======================================-->
+
+<!--<div class="container-fluid productos">
+	
+	<div class="container">
+		
+		<div class="row">
+			
+			=====================================
+			BARRA TÍTULO
+			======================================
+
+			<div class="col-xs-12 tituloDestacado">
+				
+				===============================================
+
+				<div class="col-sm-6 col-xs-12">
+					
+					<h1><small>ARTÍCULOS GRATUITOS</small></h1>
+
+				</div>
+
+				===============================================
+
+				<div class="col-sm-6 col-xs-12">
+					
+					<a href="articulos-gratis">
+						
+						<button class="btn btn-default backColor pull-right">
+							
+							VER MÁS <span class="fa fa-chevron-right"></span>
+
+						</button>
+
+					</a>
+
+				</div>
+
+				===============================================
+
+			</div>
+
+			<div class="clearfix"></div>
+
+			<hr>
+
+		</div>
+
+		=====================================
+		VITRINA DE PRODUCTOS EN CUADRÍCULA
+		======================================
+
+		<ul class="grid0">
+
+			Productos Gratis lista
+
+		</ul>
+
+		=====================================
+		VITRINA DE PRODUCTOS EN LISTA
+		======================================
+
+		<ul class="list0"  style="display:none">
+			
+			PRODUCTO GRATIS EN LISTA
+
+		</ul>
+
+	</div>
+
+</div>-->
+
+<!--=====================================
+BARRA PRODUCTOS MÁS VENDIDOS
+======================================-->
+
+<!--<div class="container-fluid well well-sm barraProductos">
+
+	<div class="container">
+		
+		<div class="row">
+			
+			<div class="col-xs-12 organizarProductos">
+
+				<div class="btn-group pull-right">
+
+					 <button type="button" class="btn btn-default btnGrid" id="btnGrid1">
+					 	
+						<i class="fa fa-th" aria-hidden="true"></i>  
+
+						<span class="col-xs-0 pull-right"> GRID</span>
+
+					 </button>
+
+					 <button type="button" class="btn btn-default btnList" id="btnList1">
+					 	
+						<i class="fa fa-list" aria-hidden="true"></i> 
+
+						<span class="col-xs-0 pull-right"> LIST</span>
+
+					 </button>
+					
+				</div>		
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>-->
+
+<!--=====================================
+VITRINA DE PRODUCTOS MÁS VENDIDOS
+======================================-->
+
+<!--<div class="container-fluid productos">
+	
+	<div class="container">
+		
+		<div class="row">
+			
+			=====================================
+			BARRA TÍTULO
+			======================================-
+
+			<div class="col-xs-12 tituloDestacado">
+				
+				===============================================
+
+				<div class="col-sm-6 col-xs-12">
+					
+					<h1><small>LO MÁS VENDIDO </small></h1>
+
+				</div>
+
+				===============================================
+
+				<div class="col-sm-6 col-xs-12">
+					
+					<a href="lo-mas-vendido">
+						
+						<button class="btn btn-default backColor pull-right">
+							
+							VER MÁS <span class="fa fa-chevron-right"></span>
+
+						</button>
+
+					</a>
+
+				</div>
+
+				===============================================
+
+			</div>
+
+			<div class="clearfix"></div>
+
+			<hr>
+
+		</div>
+
+		=====================================
+		VITRINA DE PRODUCTOS EN CUADRÍCULA
+		=====================================
+
+		<ul class="grid1">
+			
+			Productos mas vendidos cuadricula
+
+		</ul>
+
+		=====================================
+		VITRINA DE PRODUCTOS EN LISTA
+		=====================================
+
+		<ul class="list1" style="display:none">
+			
+			PRODUCTOS MAS VENDIDOS EN LISTA
+
+		</ul>
+
+	</div>
+
+</div>-->
+
+<!--=====================================
+BARRA PRODUCTOS MÁS VISTOS
+======================================-->
+
+<!--<div class="container-fluid well well-sm barraProductos">
+
+	<div class="container">
+		
+		<div class="row">
+			
+			<div class="col-xs-12 organizarProductos">
+
+				<div class="btn-group pull-right">
+
+					 <button type="button" class="btn btn-default btnGrid" id="btnGrid2">
+					 	
+						<i class="fa fa-th" aria-hidden="true"></i>  
+
+						<span class="col-xs-0 pull-right"> GRID</span>
+
+					 </button>
+
+					 <button type="button" class="btn btn-default btnList" id="btnList2">
+					 	
+						<i class="fa fa-list" aria-hidden="true"></i> 
+
+						<span class="col-xs-0 pull-right"> LIST</span>
+
+					 </button>
+					
+				</div>		
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>-->
+
+<!--=====================================
+VITRINA DE PRODUCTOS MÁS VISTOS
+======================================-->
+
+<!--<div class="container-fluid productos">
+	
+	<div class="container">
+		
+		<div class="row">
+			
+			=====================================
+			BARRA TÍTULO
+			=====================================
+
+			<div class="col-xs-12 tituloDestacado">
+				
+				===============================================
+
+				<div class="col-sm-6 col-xs-12">
+					
+					<h1><small>LO MÁS VISTO </small></h1>
+
+				</div>
+
+				===============================================
+
+				<div class="col-sm-6 col-xs-12">
+					
+					<a href="lo-mas-visto">
+						
+						<button class="btn btn-default backColor pull-right">
+							
+							VER MÁS <span class="fa fa-chevron-right"></span>
+
+						</button>
+
+					</a>
+
+				</div>
+
+				===============================================
+
+			</div>
+
+			<div class="clearfix"></div>
+
+			<hr>
+
+		</div>
+
+		=====================================
+		VITRINA DE PRODUCTOS EN CUADRÍCULA
+		=====================================
+
+		<ul class="grid2">
+			
+			Productos mas vistos en cuadricula
+
+		</ul>
+
+		=====================================
+		VITRINA DE PRODUCTOS EN LISTA
+		=====================================
+
+		<ul class="list2" style="display:none">
+			
+			PRODUCTOS MAS VISTOS EN LISTA
+
+		</ul>
+
+	</div>
+
+</div>-->
---
+++ frontend/vistas/modulos/slide.html.php
@@ -0,0 +1,40 @@
+<!--=====================================
+SLIDESHOW  
+======================================-->
+
+<div class="container-fluid" id="slide">
+	
+	<div class="row">
+		<!--=====================================
+		PAGINACIÓN
+		======================================-->
+
+		<ol id="paginacion">
+
+        	<li item="1"><span class="fa fa-circle"></span></li>
+			<li item="2"><span class="fa fa-circle"></span></li>
+			<li item="3"><span class="fa fa-circle"></span></li>
+			<li item="4"><span class="fa fa-circle"></span></li>
+
+		</ol>	
+
+		<!--=====================================
+		FLECHAS
+		======================================-->	
+
+		<div class="flechas" id="retroceder"><span class="fa fa-chevron-left"></span></div>
+		<div class="flechas" id="avanzar"><span class="fa fa-chevron-right"></span></div>
+
+	</div>
+
+</div>
+
+<center>
+	
+	<button id="btnSlide" class="backColor">
+		
+			<i class="fa fa-angle-up"></i>
+
+	</button>
+
+</center>
